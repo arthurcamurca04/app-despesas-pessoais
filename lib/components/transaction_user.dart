3e3013855d65b2742a79728f90c74b4bfba07465
Arthur <arthur.04.pb@gmail.com>
2020-03-21 17:19:40 -0300

    Included two new foldrs(models, components) and code refactoring

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,38 @@
+import 'package:despesas_pessoais/components/transaction_form.dart';
+import 'package:despesas_pessoais/components/transaction_list.dart';
+import 'package:despesas_pessoais/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 205.3,
+      date: DateTime.now(),
+    ),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm()
+      ],
+      
+    );
+  }
+}
